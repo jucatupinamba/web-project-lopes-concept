4ff0bbfdb919015f9afda0796aba97ab875c4cc4
Juarez Martins <juniorbmx13@gmail.com>
2022-10-31 23:48:19 -0300

    feat: add class Cadastro, CadastroRepository

---
+++ src/main/java/br/com/lopesconcept/repository/CadastroRepository.java
@@ -0,0 +1,9 @@
+package br.com.lopesconcept.repository;
+
+import br.com.lopesconcept.model.Cadastro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CadastroRepository extends JpaRepository<Cadastro, Long> {
+}
