82b1ab9976c7cb5862bd75e3f96017f47ba1f3c5
Juarez Martins <juniorbmx13@gmail.com>
2022-10-31 23:14:45 -0300

    feat: initial commit Fullstack Project

---
+++ src/test/java/br/com/lopesconcept/lopesconcept/LopesConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lopesconcept.lopesconcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LopesConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
